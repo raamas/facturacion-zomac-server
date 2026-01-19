c45043ed060db9c085ac3ba955ef9c4a6c9e9b6b
raamas <omardenilson007@gmail.com>
2026-01-19 10:28:28 -0500

    add more functions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import SupabaseClient from 'src/supabase-client';
 import { ConfigService } from '@nestjs/config';
 @Controller('users')
 export class UsersController {
   supabaseClient: SupabaseClient;
+  configService: ConfigService;
   constructor(configService: ConfigService) {
     this.supabaseClient = new SupabaseClient(configService);
+    this.configService = configService;
   }
   @Get()
   async getAllUsers() {
@@ -16,16 +26,29 @@ export class UsersController {
     if (error) return { error };
     return users;
   }
+
+  //For One
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    const { data, error } =
+      await this.supabaseClient.supabase.auth.admin.getUserById(id);
+    if (error) return { error };
+    return data;
+  }
   @Post()
   async createUser(
     @Body()
     body: {
+      admin_id: string;
       email: string;
       password: string;
       role: string;
       full_name: string;
     },
   ) {
+    if (body.admin_id !== this.configService.get<string>('ADMIN_ID')) {
+      return { error: 'Unauthorized' };
+    }
     const {
       data: { user },
       error,
@@ -42,14 +65,35 @@ export class UsersController {
     return user;
   }
 
-  //For One
-  @Get(':id')
-  async getUser(@Param('id') id: string) {
+  @Put(':id')
+  async updateUser(
+    @Body()
+    body: {
+      admin_id: string;
+      email: string;
+      password: string;
+      role: string;
+      full_name: string;
+    },
+    @Param('id') id: string,
+  ) {
+    if (body.admin_id !== this.configService.get<string>('ADMIN_ID')) {
+      return { error: 'Unauthorized' };
+    }
     const { data, error } =
-      await this.supabaseClient.supabase.auth.admin.getUserById(id);
+      await this.supabaseClient.supabase.auth.admin.updateUserById(id, {
+        email: body.email,
+        password: body.password,
+        user_metadata: {
+          full_name: body.full_name,
+          role: body.role,
+        },
+        email_confirm: true,
+      });
     if (error) return { error };
     return data;
   }
+
   @Delete(':id')
   async deleteUser(@Param('id') id: string) {
     const { data, error } =
